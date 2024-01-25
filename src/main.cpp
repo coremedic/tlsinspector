815760875612bea1255cf2ba43e1a896691e36ed
coremedic <ben@aptlabs.net>
2024-01-24 19:01:34 -0500

    Added TLS callback function check logic, need to fix errors getting thread context

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <winternl.h>
 #include <tlhelp32.h>
 #include <vector>
 #include <cstdio>
@@ -52,6 +53,88 @@ HANDLE* EnumThreads(IN DWORD pid, OUT int* threadCount) {
     return phThreads;
 }
 
+BOOL CheckTLSCallbackArray(IN HANDLE hProc, IN HANDLE hThread) {
+    if (!hProc || !hThread) {
+#ifdef DEBUG
+        printf("[!] Process or Thread handle is NULL\n");
+#endif
+
+        return FALSE;
+    }
+
+    ULONG_PTR               uImageBase          = NULL,
+                            uImageBaseBuffer    = NULL;
+    PIMAGE_NT_HEADERS       pImgNtHdrs          = NULL;
+    PIMAGE_DATA_DIRECTORY   pEntryTLSDataDir    = NULL;
+    PIMAGE_TLS_CALLBACK     pImgTlsCallback     = NULL;
+    CONTEXT                 ThreadContext       = {.ContextFlags = CONTEXT_ALL};
+    BOOL                    bResult             = FALSE;
+
+    if (!GetThreadContext(hThread, &ThreadContext)) {
+#ifdef DEBUG
+        printf("[!] GetThreadContext failed with error [%lu]\n", GetLastError());
+#endif
+
+        return FALSE;
+    }
+
+    size_t reserved3Offset = offsetof(PEB, Reserved3);
+    size_t elementSize = sizeof(PEB::Reserved3[0]);
+    size_t specificElementOffset = reserved3Offset + elementSize;
+    PVOID specificElementAddress = (char*)(ThreadContext.Rdx) + specificElementOffset;
+#ifdef DEBUG
+    printf("[i] PPEB Address: 0x%p \n", (void*)ThreadContext.Rdx);
+    printf("[i] Calculated Image Base Address To Be At: 0x%p \n", specificElementAddress);
+#endif
+
+    if (!ReadProcessMemory(hProc, specificElementAddress, &uImageBase, sizeof(PVOID), NULL)) {
+#ifdef DEBUG
+        printf("[!] ReadProcessMemory failed with error: [%lu]\n", GetLastError());
+#endif
+
+        return FALSE;
+    }
+
+    printf("[i] Image Base Address: 0x%p \n", (void*)uImageBase);
+
+    if (!(uImageBaseBuffer = reinterpret_cast<ULONG_PTR>(LocalAlloc(LPTR, 0x1000)))) {
+#ifdef DEBUG
+        printf("[i] LocalAlloc failed with error: [%lu] \n", GetLastError());
+#endif
+
+        return FALSE;
+    }
+
+    if (!ReadProcessMemory(hProc, (PVOID)uImageBase, (LPVOID)uImageBaseBuffer, 0x1000, NULL)) {
+#ifdef DEBUG
+        printf("[!] ReadProcessMemory failed with error: [%lu]\n", GetLastError());
+#endif
+
+        goto _CLEAN_UP;
+    }
+
+    pImgNtHdrs = (PIMAGE_NT_HEADERS)(uImageBaseBuffer + ((PIMAGE_DOS_HEADER)uImageBaseBuffer)->e_lfanew);
+    if (pImgNtHdrs->Signature != IMAGE_NT_SIGNATURE) {
+        goto _CLEAN_UP;
+    }
+
+    pEntryTLSDataDir = &pImgNtHdrs->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS];
+    if (!pEntryTLSDataDir->Size) {
+#ifdef DEBUG
+        printf("[!] Remote Process Does Not Have Any TLS Callback Function\n");
+#endif
+
+        goto _CLEAN_UP;
+    }
+
+    bResult = TRUE;
+    printf("[i] Process seems to have a TLS callback function\n");
+
+    _CLEAN_UP:
+    LocalFree(reinterpret_cast<PVOID>(uImageBaseBuffer));
+    return bResult;
+}
+
 int main() {
     HANDLE                      hProcSnapshot   = NULL;
     HANDLE*                     phThreads       = NULL;
@@ -85,8 +168,15 @@ int main() {
         if (hProcSnapshot) {
             int threadCount = 0;
             phThreads = EnumThreads(pe32.th32ProcessID, &threadCount);
-
-
+            if (phThreads != NULL) {
+                for (size_t i = 0; i < threadCount; ++i) {
+                    BOOL b = FALSE;
+                    b = CheckTLSCallbackArray(hProcSnapshot, phThreads[i]);
+                    if (b) {
+                        printf("[i] Process [%lu] seems to have a TLS callback function\n");
+                    }
+                }
+            }
         } else {
 #ifdef DEBUG
             printf("[i] No more processes to inspect\n");
@@ -102,7 +192,6 @@ int main() {
     if (hProcSnapshot) {
         CloseHandle(hProcSnapshot);
     }
-
 
     return 0;
 }
